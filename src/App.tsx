e91276865f36718dcf56a65fd810dd9b62995706
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-06-25 01:51:07 -0300

    feat: concluding component Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { TimerIcon } from "lucide-react";
+
 import { Heading } from "./components/Heading/Heading";
 import "./styles/global.css";
 import "./styles/theme.css";
 
 export const App = () => {
-  return <Heading>Olá mundo</Heading>;
+  return (
+    <>
+      <Heading>
+        Olá mundo{" "}
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+    </>
+  );
 };
