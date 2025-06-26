dbb3a090b0eafd1722ff30f0e00ed388b33d49dd
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-06-26 06:19:37 -0300

    feat: use conditionals to validate props

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -2,14 +2,19 @@ import styles from "./styles.module.css";
 
 type DefaultInputProps = {
   id: string;
-  labelText: string;
+  labelText?: string;
 } & React.ComponentProps<"input">;
 
-export const DefaultInput = ({ id, type, labelText }: DefaultInputProps) => {
+export const DefaultInput = ({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputProps) => {
   return (
     <>
-      <label htmlFor={id}>{labelText}</label>
-      <input type={type} id={id} />
+      {labelText && <label htmlFor={id}>{labelText}</label>}
+      <input type={type} id={id} {...rest} />
     </>
   );
 };
