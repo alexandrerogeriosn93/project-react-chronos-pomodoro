18ed0bfd84790fc2326714baf0aa3bcf2c8f276f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-06-26 23:05:34 -0300

    feat: create component DefaultButton and add your styles css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { PlayCircleIcon } from "lucide-react";
+
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -36,7 +39,7 @@ export const App = () => {
             <Cycles />
           </div>
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
