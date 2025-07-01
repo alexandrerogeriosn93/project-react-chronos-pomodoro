24b31a1271493e4a854d016af5a2e5787d8cebb2
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-06-30 23:09:47 -0300

    feat: add accessibility to the menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,16 +5,36 @@ import styles from "./styles.module.css";
 export const Menu = () => {
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ir para a Home"
+        title="Ir para a Home"
+      >
         <HouseIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Ver histórico"
+        title="Ver histórico"
+      >
         <HistoryIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Altera tema"
+        title="Alterar tema"
+      >
         <SunIcon />
       </a>
     </nav>
