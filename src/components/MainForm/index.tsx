2318fc22a0d03eba23203b3387a791985b8313ef
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-07-18 11:09:37 -0300

    feat: validating form and create a new Task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,18 +1,62 @@
 import { PlayCircleIcon } from "lucide-react";
+import { useRef } from "react";
 
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { TaskModel } from "../../models/TaskModel";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export const MainForm = () => {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const handleCreateNewTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current?.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa.");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      duration: 1,
+      starDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00",
+        tasks: [...prevState.tasks, newTask],
+        config: { ...prevState.config },
+      };
+    });
+  };
+
   return (
-    <form action="" className="form">
+    <form action="" className="form" onSubmit={handleCreateNewTask}>
       <div className="formRow">
         <DefaultInput
           type="text"
           id="inputTask"
           labelText="task"
           placeholder="task"
+          ref={taskNameInput}
         />
       </div>
       <div className="formRow">
