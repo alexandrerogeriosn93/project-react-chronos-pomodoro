96c8a808c15b82ec64b73cf046275a4e52ead350
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-07-29 23:03:31 -0300

    feat: create page History and add your styles css

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 
 import { AboutPomodoro } from "../../pages/AboutPomodoro";
+import { History } from "../../pages/History";
 import { Home } from "../../pages/Home";
 import { NotFound } from "../../pages/NotFound";
 
@@ -20,6 +21,7 @@ export const MainRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/history/" element={<History />} />
         <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
