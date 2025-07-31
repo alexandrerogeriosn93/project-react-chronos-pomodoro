b7584ee3a8955348b6451fb7677d4a45a2a6091d
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-07-31 17:50:34 -0300

    feat: validate form fields before saving data

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,13 +1,53 @@
 import { SaveIcon } from "lucide-react";
+import { useRef } from "react";
 
+import { showMessage } from "../../adapters/showMessage";
 import { Container } from "../../components/Container";
 import { DefaultButton } from "../../components/DefaultButton";
 import { DefaultInput } from "../../components/DefaultInput";
 import { Heading } from "../../components/Heading";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { MainTemplate } from "../../templates/MainTemplate";
 import styles from "./styles.module.css";
 
 export const Settings = () => {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  const handleSaveSettings = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    showMessage.dismiss();
+
+    const workTime = Number(workTimeInput.current?.value);
+    const shorBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+    const formErrors = [];
+
+    if (isNaN(workTime) || isNaN(shorBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push("Informe apenas números para salvar as configurações.");
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push("Informe valores entre 1 e 99 para foco.");
+    }
+
+    if (shorBreakTime < 1 || shorBreakTime > 30) {
+      formErrors.push("Informe valores entre 1 e 30 para descanso curto.");
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push("Informe valores entre 1 e 60 para descanso longo.");
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach((error) => {
+        showMessage.error(error);
+      });
+    }
+  };
+
   return (
     <MainTemplate>
       <Container>
@@ -20,15 +60,33 @@ export const Settings = () => {
         </p>
       </Container>
       <Container>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={handleSaveSettings}>
           <div className="formRow">
-            <DefaultInput id="workTime" labelText="Foco" />
+            <DefaultInput
+              id="workTime"
+              labelText="Foco"
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
-            <DefaultInput id="shortBreakTime" labelText="Descando curto" />
+            <DefaultInput
+              id="shortBreakTime"
+              labelText="Descando curto"
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
-            <DefaultInput id="longBreakTime" labelText="Descanso longo" />
+            <DefaultInput
+              id="longBreakTime"
+              labelText="Descanso longo"
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
             <DefaultButton
